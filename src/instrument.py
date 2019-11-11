f6e9289b787df2787e4863981c1e2e92c81c9e32
Asen Gochev <vuki03@mail.ru>
2019-11-11 19:21:41 +0700

    Now list of parameters changes with different elements

---
+++ src/instrument.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import QAction
+
+
+class Instrument(QAction):
+    def __init__(self, id_, name, parent):
+        super().__init__(name, parent)
+        self.id = id_
+        self.triggered.connect(self.chosen)
+        self.setObjectName(name)
+
+    def chosen(self):
+        from src.mainWindow import MainWindow
+        p: MainWindow = self.parent()
+        options = [
+            (p.colorChange, 'colorized'),
+            (p.brushSizeChanger, 'size')
+        ]
+        cur = p.i_cur
+        for i in options:
+            a = cur.execute(f"SELECT {i[1]} FROM instruments WHERE id = {self.id}").fetchall()[0][0]
+            if a is None:
+                i[0].hide()
+            else:
+                i[0].show()
+                if i[1] == 'size':
+                    p.brushSize.setValue(a)
+        p.instrumentName.setText(self.objectName())
--- src/mainWindow.py
+++ src/mainWindow.py
@@ -1,8 +1,11 @@
+import sqlite3
+
 from PIL import Image
-from PyQt5 import QtCore
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow
 
 from src.field import Field
+from src.instrument import Instrument
 from ui.mainWindowUI import Ui_MainWindow
 
 
@@ -11,9 +14,18 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         super().__init__()
         self.setupUi(self)
         self.connect_buttons()
-        self.field = Field(self.scrollAreaWidgetContents)
+        self.instruments_db = sqlite3.connect('db/instruments.db')
+        self.field = Field(self.imHolderContents)
         self.field.setObjectName("field")
         self.pictureHolder.addWidget(self.field, 0, 0, 1, 1)
+        self.i_cur = self.instruments_db.cursor()
+        ins = self.i_cur.execute("SELECT id, name FROM instruments")
+        self.instruments = []
+        for i in ins:
+            inst = Instrument(i[0], i[1], self)
+            self.instruments.append(inst)
+            self.instrumentsBar.addAction(inst)
+
 
     def connect_buttons(self):
         self.action_new.triggered.connect(self.new)
