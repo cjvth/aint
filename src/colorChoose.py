7cdf3d68a2b9490415f413819d03d536cff57eb3
Asen Gochev <vuki03@mail.ru>
2019-11-13 23:50:45 +0700

    Brush size and colors now change with widgets in left. Also, user preferences are saved to a database now

--- src/colorChoose.py
+++ src/colorChoose.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QPushButton, QGraphicsView, QWidget, QColorDialog, QGraphicsScene
 
@@ -5,8 +7,10 @@ from src.clickableGraphicsView import ClickableGraphicsView
 
 
 class ColorChoose:
-    def __init__(self, fore: ClickableGraphicsView,
-                 back: ClickableGraphicsView, swap: QPushButton):
+    def __init__(self, fore: ClickableGraphicsView, back: ClickableGraphicsView,
+                 swap: QPushButton, cursor: sqlite3.Cursor, connect):
+        self.connect = connect
+        self.cursor = cursor
         self.fore_scene = QGraphicsScene(fore.parent())
         fore.setScene(self.fore_scene)
         self.back_scene = QGraphicsScene(back.parent())
@@ -14,22 +18,31 @@ class ColorChoose:
         fore.clicked.connect(self.change_foreground)
         back.clicked.connect(self.change_background)
         swap.clicked.connect(self.swap)
-        self.fore_color = QColor(0, 0, 0)
-        self.back_color = QColor(255, 255, 255)
-        self.fore_scene.addRect(-5, -5, 37, 37, self.fore_color, self.fore_color)
-        self.back_scene.addRect(-5, -5, 37, 37, self.back_color, self.back_color)
+        self.fore_color = self.cursor.execute("""SELECT r, g, b FROM color
+        WHERE ground='fore'""").fetchall()[0]
+        self.back_color = self.cursor.execute("""SELECT r, g, b FROM color 
+        WHERE ground='back'""").fetchall()[0]
+        self.fore_scene.addRect(-5, -5, 37, 37, QColor(*self.fore_color), QColor(*self.fore_color))
+        self.back_scene.addRect(-5, -5, 37, 37, QColor(*self.back_color), QColor(*self.back_color))
 
     def change_foreground(self):
         color = QColorDialog.getColor()
         self.fore_scene.addRect(-5, -5, 37, 37, color, color)
-        self.fore_color = color
+        self.fore_color = (color.red(), color.green(), color.blue())
+        self.cursor.execute(f"UPDATE color SET (r, g, b) = {self.fore_color} WHERE ground='fore'")
+        self.connect.commit()
 
     def change_background(self):
         color = QColorDialog.getColor()
         self.back_scene.addRect(-5, -5, 37, 37, color, color)
-        self.back_color = color
+        self.back_color = (color.red(), color.green(), color.blue())
+        self.cursor.execute(f"UPDATE color SET (r, g, b) = {self.back_color} WHERE ground='back'")
+        self.connect.commit()
 
     def swap(self):
         self.fore_color, self.back_color = self.back_color, self.fore_color
-        self.fore_scene.addRect(-5, -5, 37, 37, self.fore_color, self.fore_color)
-        self.back_scene.addRect(-5, -5, 37, 37, self.back_color, self.back_color)
+        self.fore_scene.addRect(-5, -5, 37, 37, QColor(*self.fore_color), QColor(*self.fore_color))
+        self.back_scene.addRect(-5, -5, 37, 37, QColor(*self.back_color), QColor(*self.back_color))
+        self.cursor.execute(f"UPDATE color SET (r, g, b) = {self.fore_color} WHERE ground='fore'")
+        self.cursor.execute(f"UPDATE color SET (r, g, b) = {self.back_color} WHERE ground='back'")
+        self.connect.commit()
--- src/layer.py
+++ src/layer.py
@@ -4,4 +4,3 @@ from PIL import Image
 class Layer:
     def __init__(self, im: Image.Image):
         self.im = im
-
